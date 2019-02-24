59948480fd197dea9be490ec1f13628a4dbf8391
Mason Force <43055846+masforce@users.noreply.github.com>
2019-02-24 11:19:07 -0700

    final

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import {Button, Container, Row, Col, Pane, Collapse,
+import {Button, Container, Row, Col, Collapse,
     Navbar, NavbarToggler, NavbarBrand, Nav, NavItem, NavLink,
     UncontrolledDropdown,
     DropdownToggle,
@@ -9,15 +9,25 @@ import {Button, Container, Row, Col, Pane, Collapse,
 import './App.css';
 import Alert_Tutorial from "./Alert_Tutorial";
 import Button_Tutorial from "./Button_Tutorial";
-import Slider_Tutorial from "./Slider_Tutorial";
 import Card_Tutorial from "./Card_Tutorial";
 import Switch_Tutorial from "./Switch_Tutorial";
 import hackcu_image from './images/hackcu_logo.png';
 import { GithubPicker} from 'react-color';
 import Welcome_Modal from "./Welcome_Modal";
+import Slider from '@material-ui/lab/Slider';
+import PropTypes from "prop-types";
+import {withStyles} from "@material-ui/core";
 
+const styles = {
+    root: {
+        width: 300,
+    },
+    slider: {
+        padding: '22px 0px',
+    },
+};
 
-export default class Example extends React.Component {
+class Example extends React.Component {
     constructor(props) {
         super(props);
         this.updateState = this.updateState.bind(this);
@@ -27,16 +37,19 @@ export default class Example extends React.Component {
         this.toggle = this.toggle.bind(this);
         this.reset = this.reset.bind(this);
         this.toggle = this.toggle.bind(this);
+        this.handleSliderChange = this.handleSliderChange.bind(this);
         this.toggle_modal = this.toggle_modal.bind(this);
 
         this.state = {
             isOpen: false,
             username: 'And your name is...?',
-            endline: '',
-            value: '',
+            myValue: '',
             stylePath: './index2.css',
             background_color: '#82b1ff',
-            modal: true
+            modal: true,
+            value: 4,
+            message: "",
+            endline: ''
         };
     }
 
@@ -46,7 +59,12 @@ export default class Example extends React.Component {
         });
     }
     render() {
+
+        const { classes } = this.props;
+        const { value } = this.state;
+
         return (
+
             <div>
                 <link rel="stylesheet" type="text/css" href='index.css'/>
                 <Navbar style={{backgroundColor: "#edf1f7", boxShadow: '0px 0px 8px'}} light expand="md">
@@ -55,10 +73,7 @@ export default class Example extends React.Component {
                     <Collapse isOpen={this.state.isOpen} navbar>
                         <Nav className="ml-auto" navbar>
                             <NavItem>
-                                <NavLink href="/components/">Components</NavLink>
-                            </NavItem>
-                            <NavItem>
-                                <NavLink href="https://github.com/reactstrap/reactstrap">GitHub</NavLink>
+                                <NavLink href="https://github.com/masforce/HackCU/blob/master/src/App.js">GitHub</NavLink>
                             </NavItem>
                             <UncontrolledDropdown nav inNavbar>
                                 <DropdownToggle nav caret>
@@ -77,43 +92,86 @@ export default class Example extends React.Component {
                     <Welcome_Modal  toggle_modal={this.toggle_modal}
                                     modal={this.state.modal}
                                     updateState={this.updateState}
-                                   update={this.update}
-                                   value={this.state.value}/>
+                                    update={this.update}
+                                    value={this.state.myValue}/>
                 </div>
                 <div style={{background: this.state.background_color}}>
+                    <br></br>
                     <div style={{display: 'flex', justifyContent: 'center', alignContent: 'center'}}>
                         <Button color={"secondary"} onClick={this.toggle_modal}>Change Name</Button>
                     </div>
                     <div id="content" style={{backgroundColor: this.state.background}}>
                         <h1>{this.state.username}</h1>
                         <div id="forum">
+
                             <h2>See what parts of this site you can change:</h2>
-                            <Row style={{display: 'flex', justifyContent: 'center', alignItems: 'center'}}>
-                                <h5>Start by selecting a background color...</h5>
-                            </Row>
-                            <div style={{display: 'flex', justifyContent: 'center', alignItems: 'center'}}>
-                                <Row>
-                                    <GithubPicker onChangeComplete={this.handle_color}/>
-                                </Row>
+                            <br></br>
+
+                            <div id="customizer">
+
+                                <div class="customizerElement">
+                                    <Row style={{display: 'flex', justifyContent: 'center', alignItems: 'center'}}>
+                                        <h5>Start by selecting a background color...</h5>
+                                    </Row>
+                                    <br></br>
+                                    <div style={{display: 'flex', justifyContent: 'center', alignItems: 'center'}}>
+                                        <Row>
+                                            <GithubPicker onChangeComplete={this.handle_color}/>
+                                        </Row>
+                                    </div>
+                                </div>
+                                <div className="customizerElement">
+                                    <Row style={{display: 'flex', justifyContent: 'center', alignItems: 'center'}}>
+                                        <h5>Now set the size of the headings...</h5>
+                                    </Row>
+                                    <Row>
+                                        <div id="sliderComponent">
+                                            <h4 style={{fontSize: this.state.value * 10}}>HEADINGS</h4>
+                                            <div style={{display: 'flex', justifyContent: 'center', alignItems: 'center'}}>
+                                                <div className={classes.root}>
+                                                    <Slider
+                                                        classes={{ container: classes.slider }}
+                                                        value={value}
+                                                        min={1}
+                                                        max={10}
+                                                        step={1}
+                                                        onChange={this.handleSliderChange}
+                                                    />
+                                                    <p>This is what the code looks like:</p>
+                                                    <div id={"seeSliderCode"}>
+                                                        {this.state.message}
+                                                    </div>
+                                                </div>
+                                            </div>
+                                        </div>
+                                    </Row>
+                                </div>
+
                             </div>
+
                             <div id="column">
+                                <h4 style={{fontSize: this.state.value * 10}}>BUTTONS</h4>
+                                <hr></hr>
                                 <div id="buttonComponent">
                                     <Button_Tutorial ref="button_tutorial"/>
                                 </div>
                                 <br/>
-                                <div id="alertComponent">
+                                <h4 style={{fontSize: this.state.value * 10}}>ALERTS</h4>
+                                <hr></hr>
+                                <div id="alertComponent" style={{marginTop: '40px'}}>
                                     <Alert_Tutorial ref="alert_tutorial"/>
                                 </div>
                                 <br/>
-                                <div id="cardComponent">
+                                <h4 style={{fontSize: this.state.value * 10}}>CARDS</h4>
+                                <hr></hr>
+                                <div id="cardComponent" style={{marginTop: '40px'}}>
                                     <Card_Tutorial ref="card_tutorial"/>
                                 </div>
+
                                 <br/>
-                                <div id="sliderComponent">
-                                    <Slider_Tutorial ref="slider_tutorial"/>
-                                </div>
-                                <br/>
-                                <div id="switchComponent">
+                                <h4 style={{fontSize: this.state.value * 10}}>SWITCH</h4>
+                                <hr></hr>
+                                <div id="switchComponent" style={{marginTop: '40px'}}>
                                     <Switch_Tutorial ref="switch_tutorial"/>
                                 </div>
                             </div>
@@ -131,13 +189,8 @@ export default class Example extends React.Component {
         );
     }
 
-    toggle_modal() {
-        this.setState(prevState => ({
-            modal: !prevState.modal
-        }));
-    }
-
     reset() {
+        console.log('Reset was called');
         this.refs.button_tutorial.reset();
         this.refs.alert_tutorial.reset();
         this.refs.card_tutorial.reset();
@@ -145,12 +198,18 @@ export default class Example extends React.Component {
             {
                 isOpen: false,
                 username: 'And your name is...?',
-                endline: 'Thanks for visiting!',
-                value: '',
+                value: '4',
                 stylePath: './index2.css',
                 background_color: '#82b1ff',
+                endline: 'Thanks for visiting!'
             }
         );
+    }
+
+    toggle_modal() {
+        this.setState(prevState => ({
+            modal: !prevState.modal
+        }));
     }
 
     handle_color(color) {
@@ -161,21 +220,24 @@ export default class Example extends React.Component {
     }
 
     update(e) {
-        this.setState({value: e.target.value});
+
+        this.state.myValue = e.target.value;
+        this.setState({myValue: e.target.value});
+
     }
 
     updateState() {
-        if (this.state.value != "") {
+        if (this.state.myValue != "") {
             this.setState({
-                username: 'Nice to meet you, ' + this.state.value + '!',
-                endline: 'Thanks for visiting, ' + this.state.value + '!',
-                value: ""
+                username: 'Nice to meet you, ' + this.state.myValue + '!',
+                endline: 'Thanks for visiting, ' + this.state.myValue + '!',
+                myValue: ""
             });
         }
         else {
             this.setState({
                 username: 'You didn\'t type anything.',
-                value: ""
+                myValue: ""
             });
         }
     }
@@ -184,4 +246,23 @@ export default class Example extends React.Component {
         this.setState({background: '#ffff'});
     }
 
+    handleSliderChange = (event, value) => {
+        this.setState({value: value,
+            message: "<Slider\n" +
+                "                classes={{ container: classes.slider }}\n" +
+                "                value={" + this.state.value + "}\n" +
+                "                min={0}\n" +
+                "                max={10}\n" +
+                "                step={1}\n" +
+                "                onChange={this.handleSliderChange}\n" +
+                "/>"});
+    };
+
+
+
 }
+Example.propTypes = {
+    classes: PropTypes.object.isRequired,
+};
+
+export default withStyles(styles)(Example);
